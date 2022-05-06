2eb85f3b034a8bdca367483f060123f7f6a6ed7f
dplinux-automation <105081826+dplinux-automation@users.noreply.github.com>
2022-05-07 01:28:58 +0200

    Update Alt-Enter-Automation.ps1

--- Alt-Enter-Automation.ps1
+++ Alt-Enter-Automation.ps1
@@ -1,12 +1,19 @@
 ﻿#Variable names
+ 
 $user = "Per-a" #change to your user <--------
 $RobloxVersion = "C:\Users\$user\AppData\Local\Roblox\Versions"
 $RobloxVersionLatest = Get-ChildItem -Directory -Path $RobloxVersion | Sort-Object LastWriteTime -Descending | Select-Object -first 1
 $ClientSettingLocation = $RobloxVersion + "\" + $RobloxVersionLatest.Name
 
+
 #Creation of Clientsettings and ClientAppSettings.json
+
 try {
+
 New-item -ItemType Directory -Path $ClientSettingLocation -Name ClientSettings
 $ClientSettingLocation = $RobloxVersion + "\" + $RobloxVersionLatest.Name +"\" + "ClientSettings"
 New-item -ItemType File -Path $ClientSettingLocation -Name ClientAppSettings.json -Value '{"FFlagHandleAltEnterFullscreenManually":"False"}'}
+
 catch { "An error occurred. Make sure to change your user at line 2" }
+
+
